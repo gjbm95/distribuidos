81b76dca6abc78e5ed2b68c83a8be8bd22d6dc1b
Carlos Valero <kilordpepo@gmail.com>
2017-12-10 15:03:55 -0400

    Menu inicial
    Menu para escoger el rol de ingreso

--- ProyectoDistribuidos/src/proyectodistribuidos/ProyectoDistribuidos.java
+++ ProyectoDistribuidos/src/proyectodistribuidos/ProyectoDistribuidos.java
@@ -5,6 +5,8 @@
  */
 package proyectodistribuidos;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Junior
@@ -15,7 +17,42 @@ public class ProyectoDistribuidos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bienvendo al proyecto de distribuidos");
+        System.out.println("Realizado por:");
+        System.out.println("Carlos Valero");
+        System.out.println("Garry Bruno");
+        System.out.println("");
+        System.out.println("Con que rol deseas entrar?");
+        System.out.println("Presiona 1 para entrar como Cliente");
+        System.out.println("Presiona 2 para entrar como Servidor");
+        System.out.println("Presiona 3 para entrar como Servidor Fantasma(SOLO 1)");
+        System.out.println("Presiona 4 para Salir");
+        System.out.println("Ingresa tu opcion: ");
+        int i = sc.nextInt();
+        
+        switch(i){
+           
+            case 1:{
+            // LOGICA CLIENTE
+            break;
+            }
+            case 2: {
+                //LOGICA SERVIDOR
+                break;
+            }
+            case 3:{
+            // LOGICA SERVIDOR FANTASMA
+            break;
+            }
+            case 4: {
+            // SALIR
+            break;
+            }
+            default:
+                System.out.println("Opcion erronea, saliendo del programa...");
+            }
+        
     }
     
 }
