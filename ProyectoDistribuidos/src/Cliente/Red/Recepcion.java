f9e9874dc7e49eb6753c759be12d305c30ccac06
Garry Jr. Bruno <garryjrbruno@hotmail.com>
2017-12-10 13:32:30 -0400

    Inicio del Proyecto

---
+++ ProyectoDistribuidos/src/Cliente/Red/Recepcion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cliente.Red;
+
+import Servidor.Red.*;
+
+/**
+ *
+ * @author Junior
+ */
+public class Recepcion {
+    
+}
