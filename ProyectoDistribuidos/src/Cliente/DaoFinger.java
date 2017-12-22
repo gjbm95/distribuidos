f849cbd784513916d522bb1625c5935871409cf2
Garry Jr. Bruno <garryjrbruno@hotmail.com>
2017-12-22 13:21:25 -0400

    Acomodando tabla finger

--- ProyectoDistribuidos/src/Cliente/DaoFinger.java
+++ ProyectoDistribuidos/src/Cliente/DaoFinger.java
@@ -41,7 +41,7 @@ public class DaoFinger {
     */
     public void agregarRecurso(Recurso archivo){
         
-        if (obtenerRecurso(archivo.getId())==null){ 
+        if (new DaoC().obtenerRecurso(archivo.getId())==null){ 
         File xmlFile = new File(filelocation);
         Document document = null;
         
